e7e41d5094ed34e916a7a898fe71554f92e6251a
lcarrafabr <lcarrafa.br@gmail.com>
2020-01-19 09:19:00 -0300

    Classes CSS dinamicas com ngClass

---
+++ css_dinamico_ngclass/src/app/funcionario-card/funcionario-card.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-card',
+  templateUrl: './funcionario-card.component.html',
+  //styleUrls: ['./funcionario-card.component.css']
+  styles: [`
+    .card-body{
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
+})
+export class FuncionarioCardComponent{
+
+  @Input() funcionario: any;
+
+  //getClassesCss(){
+    //return ['badge', 'badge-primary'];
+  //}
+
+  isAdmin(){
+
+    return false;
+  }
+
+  getEstilosCartao(){
+
+    return {
+      //'border-width.px' : this.funcionario.id + 'px', //pode usar dessa forma ou da forma de baixo
+      'border-width' : this.funcionario.id + 'px',
+      backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+    };
+  }
+
+
+}
