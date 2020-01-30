59e7ba09039b5e47188a6b9011c6d0d8c85ee207
lcarrafabr <lcarrafa.br@gmail.com>
2020-01-30 17:50:47 -0300

    Exportando API da directiva para o template

---
+++ directiva_para_template/src/app/funcionario-form/funcionario-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-form',
+  templateUrl: './funcionario-form.component.html',
+  styleUrls: ['./funcionario-form.component.css']
+})
+export class FuncionarioFormComponent  {
+
+  ultimoId = 0;
+  nome = 'Luciano';
+  adicionado = false;
+  @Output() funcionarioAdicionado = new EventEmitter();
+
+  adicionar(nome: string){
+
+    this.adicionado = true;
+
+    const funcionario = {
+      id: ++this.ultimoId,
+      nome: this.nome
+    };
+
+    this.funcionarioAdicionado.emit(funcionario);
+  }
+
+}
