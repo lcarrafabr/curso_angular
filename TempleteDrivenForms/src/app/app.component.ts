859a2485c46ca6d305aad5d1bce221f9087e441e
lcarrafabr <lcarrafa.br@gmail.com>
2020-05-24 18:32:42 -0300

    Criando formularios

--- TempleteDrivenForms/src/app/app.component.ts
+++ TempleteDrivenForms/src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+
+    nome: string;
+    email: string;
+    profissao: string;
+  }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +15,21 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
+  cliente = new Cliente();
+
   profissoes = ['Programador', 'Engenheiro', 'Contador', 'Arquiteto'];
 
   salvar(form: NgForm) {
-    console.log('Salvando...');
+    this.cliente.nome = form.value.primeiroNome;
+    this.cliente.email = form.value.emailAdress;
+    this.cliente.profissao = form.value.profissao;
+
     console.log(form);
+    // console.log(form.value);
+    // console.log(this.cliente);
+
+    this.cliente = new Cliente();
+    form.reset({ profissao: ''});
   }
 
 }
